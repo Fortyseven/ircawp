2bc19767d0cd6d669e3335f8b81c97570b138bb1
Fortyseven <fortyseven@network47.org>
2022-12-04 06:36:43 -0500

    Initial commit

---
+++ bot.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+import re
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+import logging
+from ircawp import IrcAwp
+import dotenv
+
+print("=-=-=-=-= BOOTING =-=-=-=-=")
+config = dotenv.dotenv_values(".env")
+
+logging.basicConfig(level=logging.ERROR)
+
+ircawp = None
+
+
+app = App(
+    token=config['SLACK_BOT_TOKEN']
+)
+
+
+@app.event("app_mention")
+def event_test(event, message, client, say, body):
+    user_id = event["user"]
+    regex = r"(<.*> )(.*)"
+
+    matches = re.match(regex, event['text'], re.MULTILINE)
+
+    prompt = matches[2]
+    print(f'===============================')
+    print(f'GPT2 Prompt >> "{prompt}"')
+
+    response = ircawp.query(prompt)
+
+    print(f'            << "{response}"')
+    print(f'===============================')
+
+    say(f"<@{user_id}>: {response}")
+
+
+if __name__ == "__main__":
+    ircawp = IrcAwp()
+    SocketModeHandler(app, config['SLACK_APP_TOKEN']).start()
---
+++ cli.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import sys
+from rich import print
+import ircawp
+
+prompt = ' '.join(sys.argv[1:])
+
+if not prompt:
+    print("Huh?")
+    os._exit(-1)
+
+
+ircawp = ircawp.IrcAwp()
+
+print(ircawp.query(prompt))
