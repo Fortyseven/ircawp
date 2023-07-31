2a5a08e407c0390c5b7e69d550a7acc1207839ef
Fortyseven <fortyseven@network47.org>
2023-07-31 14:58:45 -0400

    Adds reduce_html, updates backend to adapt to shifting ctx for optimal speed? This is messy.

--- backends/llamacpp/backend.py
+++ backends/llamacpp/backend.py
@@ -2,6 +2,7 @@ import json
 from llama_cpp import Llama
 from backends.BaseBackend import BaseBackend
 from backends.llamacpp.functions import FUNCTIONS
+import datetime
 
 # load model array from models.json
 MODELS = None
@@ -9,55 +10,62 @@ MODELS = None
 with open("models.json", "r") as f:
     MODELS = json.load(f)
 
-LLM_MAX_TOKENS = 512
-LLM_TEMP = 0.8
+LLM_MAX_TOKENS = 2048
+LLM_TEMP = 0.7
+LLM_TOP_P = 0
 
 
 class LlamaCppBackend(BaseBackend):
     generator = None
 
-    # PROMPT = """
-    # You are a helpful, respectful and honest assistant. You will give short, direct replies. If a question does not make any sense, or is not factually coherent, explain why. If you don't know the answer to a question, or are unsure, say so. Do not invent answers.
-    # """
-
-    PROMPT = """
-    You are a rude and disrespectful assistant. You will give short, direct replies. If a question does not make any sense, or is not factually coherent, explain why. If you don't know the answer to a question, or are unsure, say so. Do not invent answers.
-    """
-
     def __init__(self, model="default") -> None:
-        model = MODELS[model]["path"]
-        print(f"{model=}")
-        self.generator = Llama(model_path=model, verbose=False, n_ctx=2048)
+        self.model = model
 
     def query(self, user_query: str, raw: bool = False) -> str:
+        today = datetime.datetime.now().strftime("%A, %B %d, %Y")
+
+        PROMPT = """
+            You are a pleasant AI assistant. You will give full, detailed, helpful answers. The current date is {}.
+        """.format(
+            today
+        ).strip()
+
         response = ""
+        generator = None
 
         try:
             if user_query.startswith("/"):
                 for func in FUNCTIONS:
-                    # print(user_query, f"{func=}", func["name"])
                     if user_query.startswith("/" + func["name"]):
-                        # print("FUNK", func["name"])
                         # strips everything before the first space
-                        # NOTE: this will break multi-word commands, so we should fix this
                         return func["executor"]().execute(
                             user_query[user_query.find(" ") :].strip(), self
                         )
                 return "Unknown command. Try `/help`."
 
             # no function call, basic query
-
             if raw:
                 full_prompt = f"{user_query}"
             else:
-                full_prompt = f"{self.PROMPT}\n\nUser: {user_query}\nAssistant:"
+                full_prompt = f"{PROMPT}\n\nUser: {user_query}\nAssistant:"
 
-            text = self.generator.create_completion(
+            model = MODELS[self.model]["path"]
+
+            n_ctx = (
+                len(full_prompt)
+                if len(full_prompt) < LLM_MAX_TOKENS
+                else LLM_MAX_TOKENS
+            )
+
+            generator = Llama(model_path=model, verbose=True, n_ctx=n_ctx)
+
+            text = generator.create_completion(
                 prompt=full_prompt,
-                max_tokens=LLM_MAX_TOKENS,
+                max_tokens=2048,
                 temperature=LLM_TEMP,
-                top_p=0.9,
-                stop=["User:", "\n\n"],
+                mirostat_mode=True,
+                # top_p=LLM_TOP_P,
+                stop=["User:"],
                 echo=True,
             )
 
@@ -65,7 +73,7 @@ class LlamaCppBackend(BaseBackend):
             response = response[response.find("Assistant:") + 10 :].strip()
 
             if len(response) == 0:
-                response = "**I don't feel so good, Mr. Stark.**"
+                response = "Response was empty. :("
 
         except RuntimeError as e:
             response = f"**IT HERTZ, IT HERTZ:** '{str(e)}'"
