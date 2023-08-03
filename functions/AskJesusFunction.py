c2c27df95802e3f9d67e47b5beb99b80fbbb9f53
Fortyseven <fortyseven@network47.org>
2023-08-03 04:22:16 -0400

    Adds "AskJesus" command

---
+++ functions/AskJesusFunction.py
@@ -0,0 +1,33 @@
+"""
+Bot function to summarize a web page using a smaller,
+faster model than the default chat model.
+"""
+
+import json
+import requests
+
+from backends.BaseBackend import BaseBackend
+
+from langchain.docstore.document import Document
+from langchain.chains.summarize import load_summarize_chain
+from langchain import LlamaCpp
+from langchain.text_splitter import CharacterTextSplitter
+
+from lib.reduce import reduce_html
+from lib.config import config
+
+
+def execute(query: str, backend: BaseBackend) -> str:
+    if not query.strip():
+        return "No question for our Lord and Savior?"
+
+    try:
+        full_prompt = f"""
+        You are Jesus Christ. Jesus Christ is an AI version of the historical figure who lived in the first century AD. You will be asked questions from people wanting your unique spiritual perspective. You will repeat the question, and then give a short, brief, valuable, compassionate, wholesome and helpful answer. Use examples from The Bible, quote relevant passages. It is important to encourage love, compassion, and a wholesomeness in the analysis. Remember, keep the response brief. It is essential to remember to keep the response concise and focused. This will allow for maximum impact and understanding for those seeking guidance.
+
+        The user's question is: {query.strip()}
+        """.strip()
+
+        return backend.query(full_prompt, raw=False)
+    except Exception as e:
+        return "HOLY PROBLEMS: " + str(e)
