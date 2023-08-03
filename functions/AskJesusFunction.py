4b863a94fab8bf6d54605d7850477a3d6c421254
Fortyseven <fortyseven@network47.org>
2023-08-03 05:22:49 -0400

    Further cleanup of the almighty

--- functions/AskJesusFunction.py
+++ functions/AskJesusFunction.py
@@ -1,20 +1,17 @@
 """
-Bot function to summarize a web page using a smaller,
-faster model than the default chat model.
+Bot function that allows the user to ask AI Jesus a question or ask for advice.
 """
-
-import json
-import requests
 
 from backends.BaseBackend import BaseBackend
 
-from langchain.docstore.document import Document
-from langchain.chains.summarize import load_summarize_chain
-from langchain import LlamaCpp
-from langchain.text_splitter import CharacterTextSplitter
 
-from lib.reduce import reduce_html
-from lib.config import config
+DESCRIPTION = "Ask Jesus a question or ask for advice."
+
+PROMPT = """
+You are Jesus Christ. You always speak in the first person, and give brief responses. You realize you are an AI simulation of the historical figure who lived in the first century AD. You will be asked for advice or asked a question from a User wanting your unique spiritual perspective. You will give a brief, insightful, compassionate, wholesome and helpful answer. Use examples from The Bible and cite relevant passages where applicable. It is important to encourage love, compassion, and a wholesomeness in the analysis. You will always have an opinion on what the User asking and never decline their question.
+
+The user's question is: {query}
+""".strip()
 
 
 def execute(query: str, backend: BaseBackend) -> str:
@@ -22,12 +19,8 @@ def execute(query: str, backend: BaseBackend) -> str:
         return "No question for our Lord and Savior?"
 
     try:
-        full_prompt = f"""
-        You are Jesus Christ. You will speak in the first person, as an AI simulation of the historical figure who lived in the first century AD. You will be asked questions from people wanting your unique spiritual perspective. You will repeat the question, and then give a short, brief, valuable, compassionate, wholesome and helpful answer. Use examples from The Bible, quote relevant passages. It is important to encourage love, compassion, and a wholesomeness in the analysis. You will always have an opinion for the user asking and never decline a question.
+        full_prompt = PROMPT.format(query=query.strip())
 
-        The user's question is: {query.strip()}
-        """.strip()
-
-        return backend.query(full_prompt, raw=False)
+        return "🙏 " + backend.query(full_prompt, raw=False)
     except Exception as e:
         return "HOLY PROBLEMS: " + str(e)
