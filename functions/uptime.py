8066f2d4ccb9adffe14bf73dcd01b338da0fec39
Fortyseven <fortyseven@network47.org>
2023-08-04 23:50:55 -0400

    Adds initial uptime command

---
+++ functions/uptime.py
@@ -0,0 +1,19 @@
+import datetime
+from backends import BaseBackend
+
+
+TRIGGERS = ["uptime", "stats"]
+DESCRIPTION = "Get the bot's uptime and stats."
+
+# get now
+START_TIME = datetime.datetime.now()
+
+
+def execute(query: str, backend: BaseBackend) -> str:
+    from datetime import datetime
+    from datetime import timedelta
+
+    now = datetime.now()
+    uptime = now - START_TIME
+
+    return f"Uptime: {uptime}"
