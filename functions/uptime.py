c97f2d31a7df8da956cfab7c0ed2ab5c26f93592
Fortyseven <fortyseven@network47.org>
2023-08-05 00:06:12 -0400

    Uptime output formatting

--- functions/uptime.py
+++ functions/uptime.py
@@ -8,6 +8,8 @@ DESCRIPTION = "Get the bot's uptime and stats."
 # get now
 START_TIME = datetime.datetime.now()
 
+# make START_TIME prettier
+
 
 def execute(query: str, backend: BaseBackend) -> str:
     from datetime import datetime
@@ -16,9 +18,12 @@ def execute(query: str, backend: BaseBackend) -> str:
     now = datetime.now()
     uptime = now - START_TIME
 
+    uptime = str(uptime).split(".")[0]
+    start_time = START_TIME.strftime("%Y-%m-%d %H:%M:%S")
+
     return f"""
     - Uptime: {uptime}
-    - Last started: {START_TIME}
+    - Last started: {start_time}
     - Current model: {backend.model}
     - Last query time: {backend.last_query_time or "None yet"}
     """.strip()
