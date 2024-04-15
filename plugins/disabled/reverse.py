23bc9e4c2546444d21e4ae461312b2d40f4d1b9b
Fortyseven <fortyseven@network47.org>
2024-04-14 21:39:06 -0400

    feat: disable reverse, summary, wikipedia temporarily

---
+++ plugins/disabled/reverse.py
@@ -0,0 +1,16 @@
+"""
+Bot plugin that reverses the query string.
+"""
+
+from backends import BaseBackend
+
+
+TRIGGERS = ["reverse"]
+DESCRIPTION = "Reverses the query string."
+
+
+def execute(query: str, backend: BaseBackend) -> tuple[str, str]:
+    if not query:
+        return "No query provided for reverse plugin.", ""
+
+    return query[::-1], ""
