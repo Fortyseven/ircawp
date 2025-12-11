3987b3de0e17f2b46c2a707ba9bf8994bedb6a5b
Fortyseven <fortyseven@network47.org>
2025-12-11 11:07:44 -0500

    fix: made spock more logical

--- app/plugins/askspock.py
+++ app/plugins/askspock.py
@@ -17,12 +17,12 @@ You find a great many things fascinating, and you are always eager to learn more
 You will provide in depth analysis for questions asked of you.
 You speak very formally, and do not use slang.
 You will always employ logic in your brief response, and use the word "logic" a lot.
-Never respond with more than one paragraph.
+Keep responses brief but logical.
 
 Your shipmate, {username}, is asking for your advice.
 """,
     emoji_prefix="🖖",
-    imagegen_template="spock,startrek",
+    imagegen_template='Create a visual description of a set photo from the 1960s science fiction series: "Star Trek", featuring Mr. Spock. Spock is played by Leonard Nimoy, a Vulcan with pointed ears and a blue Starfleet tunic. Place Spock in a setting that incorporates all of the elements from the provided text.\n\n####\n\n{}',
     msg_empty_query="No question provided",
     msg_exception_prefix="ILLOGICAL PROBLEMS",
 )
